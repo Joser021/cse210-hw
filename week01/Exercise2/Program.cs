cc4d44edb5518f0156a1bb946dfc0bb6c5eaa445
joser021 <joser.dev021@gmail.com>
2025-07-02 12:29:47 -0300

    adding the exercise2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,69 @@
 using System;
+using System.ComponentModel;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        // A >= 90
+        // B >= 80
+        // C >= 70
+        // D >= 60
+        // F < 60
+
+        Console.Write("What is you grade percentage? ");
+        string textPercentage = Console.ReadLine();
+        int gradePercentage = int.Parse(textPercentage);
+
+        string grade = "";
+        // gets the grade result
+        if (gradePercentage >= 90)
+        {
+            grade = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            grade = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            grade = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            grade = "D";
+        }
+        else if (gradePercentage <= 60)
+        {
+            grade = "F";
+        }
+
+        string sign = "";
+        // gets the sign
+        if (grade == "F" || gradePercentage >= 93)
+        {
+            sign = "";
+        }
+        else if (gradePercentage % 10 < 3)
+        {
+            sign = "-";
+        }
+        else if (gradePercentage % 10 >= 7)
+        {
+            sign = "+";
+        }
+
+        Console.WriteLine($"Your Final grade is: {grade}{sign}");
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("You passed! Congratulations");
+        }
+        else
+        {
+            Console.WriteLine("I am sorry, you need more than 70 to pass the class.");
+            Console.WriteLine("Good luck next time!");
+        }
     }
 }
