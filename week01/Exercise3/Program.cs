6951e1e9e7e1c39a8c4f95aa6ecbaabcd283cb1c
joser021 <joser.dev021@gmail.com>
2025-07-02 18:18:29 -0300

    adding exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,51 @@
 using System;
+using System.Dynamic;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string response = "yes";
+        do
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 100);
+            int guess = -1;
+
+            Console.WriteLine("Guess the number (1 to 100)");
+            while (guess != magicNumber)
+            {
+                // prompt the user for the guess
+                Console.Write("What is the magic number? ");
+                guess = int.Parse(Console.ReadLine());
+
+                // get if the number is right
+                if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            }
+
+            // check if you want to continue
+            Console.Write("Do you wanto to try again? (yes/no): ");
+            response = (Console.ReadLine());
+            if (response != "yes")
+            {
+                Console.Write("Thank you for playing!");
+            }
+            else
+            {
+                continue;
+            }
+        } while (response == "yes");
     }
 }
