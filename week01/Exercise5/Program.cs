f64e633153e97609fed54bb034a3fc3f75299358
joser021 <joser.dev021@gmail.com>
2025-07-03 20:32:06 -0300

    updating exercise 5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,11 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        // DisplayWelcome - Displays the message, "Welcome to the Program!"
-        // PromptUserName - Asks for and returns the user's name (as a string)
-        // PromptUserNumber - Asks for and returns the user's favorite number (as an integer)
-        // SquareNumber - Accepts an integer as a parameter and returns that number squared (as an integer)
-        // DisplayResult - Accepts the user's name and the squared number and displays them.
         DisplayWelcome();
         string userName = PromptUserName();
         int favoriteNumber = PromptUserNumber();
