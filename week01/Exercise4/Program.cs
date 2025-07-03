9f7c1f5241e0faaf19937b8333418d1908f054df
joser021 <joser.dev021@gmail.com>
2025-07-02 22:58:13 -0300

    adding the exercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        while (true)
+        {
+            Console.Write("Enter number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number == 0)
+            {
+                Console.WriteLine("Thank you for using!");
+                break;
+            }
+            numbers.Add(number);
+        }
+
+        // get the sum and the largest number
+        int sum = 0;
+        int largest = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+            if (number > largest)
+            {
+                largest = number;
+            }
+        }
+
+        // get the average
+        int numbersLength = numbers.Count();
+        double average = ((float)sum) / numbersLength;
+
+
+        Console.WriteLine(sum);
+        Console.WriteLine(average);
+        Console.WriteLine(largest);        
     }
 }
