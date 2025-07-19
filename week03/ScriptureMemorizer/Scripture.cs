3f6dd46a8ce12949b4a8c7da9b479752cf6f2365
joser021 <joser.dev021@gmail.com>
2025-07-19 19:52:59 -0300

    adding comment

--- week03/ScriptureMemorizer/Scripture.cs
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -5,12 +5,14 @@ public class Scripture
     Reference _reference;
     List<Word> _words;
 
+    // removes spaces around each word and add to the list
     public Scripture(Reference Reference, string text)
     {
         _reference = Reference;
         _words = text.Split(' ').Select(word => new Word(word)).ToList();
     }
 
+    // hides the word in the text
     public void HideRandomWords(int numberToHide)
     {
         List<Word> showedWords = _words.Where(word => !word.IsHidden()).ToList();
@@ -26,12 +28,14 @@ public class Scripture
         }
     }
 
+    // displays the scripture
     public string GetDisplayText()
     {
         var displayWord = _words.Select(word => word.GetDisplayText());
         return $"{_reference.GetDisplayText()} {string.Join(" ", displayWord)}";
     }
 
+    // check is the text is completly hidden
     public bool IsCompletelyHidden()
     {
         return _words.All(word => word.IsHidden());
